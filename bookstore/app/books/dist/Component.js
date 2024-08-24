5972d54eea126b9c275767752e5a20297377f100
Shanthakumar Krishnaswamy <shanthakumar.krishnaswamy@sap.com>
2024-08-24 20:49:25 +0000

    Fiori App

---
+++ bookstore/app/books/dist/Component.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/fe/core/AppComponent"],function(o){"use strict";return o.extend("com.sap.bookshop.books.Component",{metadata:{manifest:"json"}})});
+//# sourceMappingURL=Component.js.map
